4dceb967f1555ec253b72ed00dc1c2c6bd56d494
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-03 06:40:53 -0600

    Update SiteSwitcher.tsx

--- docs/components/SiteSwitcher.tsx
+++ docs/components/SiteSwitcher.tsx
@@ -56,8 +56,8 @@ function SiteSwitcher() {
           { "hover:text-black dark:hover:text-white": site }
         )}
       >
-        <SiteSwitcherLink href="/repo" text="Repo" isActive={site === "repo"} />
-        <SiteSwitcherLink href="/pack" text="Pack" isActive={site === "pack"} />
+        <SiteSwitcherLink href="/repo" text="Solutions" isActive={site === "repo"} />
+        <SiteSwitcherLink href="/pack" text="Basilisk" isActive={site === "pack"} />
       </span>
     </div>
   );
