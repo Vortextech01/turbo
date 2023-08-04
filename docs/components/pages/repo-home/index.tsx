5f5b2bd6c368e986921d5bcf836c84d698f5df41
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-04 09:16:27 -0600

    Update index.tsx

--- docs/components/pages/repo-home/index.tsx
+++ docs/components/pages/repo-home/index.tsx
@@ -3,7 +3,7 @@ import { RepoFeatures } from "./RepoFeatures";
 import { RepoLetter } from "./RepoLetter";
 import { GradientSectionBorder } from "../home-shared/GradientSectionBorder";
 import { LandingPageGlobalStyles } from "../home-shared/GlobalStyles";
-import Chatbot from ".../components/Chatbot"; // add this line to import the Chatbot component
+import Chatbot from ".../Chatbot"; // add this line to import the Chatbot component
 
 export default function Home() {
   return (
