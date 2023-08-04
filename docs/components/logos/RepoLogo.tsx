0ae3cfcf5c970b1dc93a93f6df915fd409e173bf
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-04 04:29:47 -0600

    Update RepoLogo.tsx

--- docs/components/logos/RepoLogo.tsx
+++ docs/components/logos/RepoLogo.tsx
@@ -1,6 +1,6 @@
 const RepoLogo = (props) => (
-  <div {...props} style={{fontSize: '2em', fontWeight: 'bold'}}>
-    Servicios
+  <div {...props} style={{fontSize: '1em', fontWeight: 'bold'}}>
+    SERVICES
   </div>
 );
 
