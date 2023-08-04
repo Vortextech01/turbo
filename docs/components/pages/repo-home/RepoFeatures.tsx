63a3332b1c59126d9703431444b76ef942839d4d
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-04 11:44:07 -0600

    Update RepoFeatures.tsx

--- docs/components/pages/repo-home/RepoFeatures.tsx
+++ docs/components/pages/repo-home/RepoFeatures.tsx
@@ -6,8 +6,8 @@ export function RepoFeatures() {
   return (
     <FadeIn className="py-16 md:py-24 lg:py-32">
       <FeaturesBento
-        header="Why Turborepo?"
-        body="Turborepo reimagines build system techniques used by Facebook and Google to remove maintenance burden and overhead."
+        header="Why Sapiens?"
+        body="Because our goal is to build way more than just lenguage models, we are compromised to develop machines that think by themselves."
         features={REPO_HOME_FEATURES}
       />
     </FadeIn>
