e3342aa9d339f27483726103f1a092e316879b39
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-07 09:31:08 -0600

    Update Vercel.tsx

--- docs/components/logos/Vercel.tsx
+++ docs/components/logos/Vercel.tsx
@@ -1,7 +1,7 @@
 const Vercel = ({ height = 80 }: { height?: number }) => (
   <svg height={height} viewBox="0 0 283 256" fill="none">
     <title>Sapiens IA</title>
-    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black" font-size="50">🧬Sapiens IA</text>
+    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="blue" font-size="50">🧬Sapiens IA</text>
   </svg>
 );
 
