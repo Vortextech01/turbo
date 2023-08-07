f3e4fe88ffbc52352c6ec384f07d5ad26035778f
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-07 08:05:53 -0600

    Update RepoFeatures.tsx

--- docs/components/pages/repo-home/RepoFeatures.tsx
+++ docs/components/pages/repo-home/RepoFeatures.tsx
@@ -6,8 +6,8 @@ export function RepoFeatures() {
   return (
     <FadeIn className="py-16 md:py-24 lg:py-32">
       <FeaturesBento
-        header="Why Sapiens?"
-        body="Because our goal is to build way more than just lenguage models, we are compromised to develop machines that think by themselves."
+        header="Porque Sapiens?"
+        body="Porque nuestro objetivo es construir mas que solo robots o modelos de lenguaje. Estamos comprometidos a crear y entrenar inteligencias artificiales que piensen por si mismas."
         features={REPO_HOME_FEATURES}
       />
     </FadeIn>
